5cc768444833cdb8a5dbe163c26040dbaae6fd51
Leon28ft <89437589+Leon28ft@users.noreply.github.com>
2021-08-24 20:40:38 -0300

    go back

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import router from './routes/index.routes.ts';
 
