1a8c01e0bf773aebcfe9756ae3b8979d88e39268
Leon28ft <89437589+Leon28ft@users.noreply.github.com>
2021-08-25 20:08:33 -0300

    oak

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { Application, Router, Request, Response } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+export { Application, Router, Request, Response } from "https://deno.land/x/oak/mod.ts";
